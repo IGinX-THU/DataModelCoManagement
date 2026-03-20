99b0f71c275761e1dacf8690277569d7a40eb9a7
mort4869 <2268589389@qq.com>
2026-03-20 23:46:29 +0800

    fix:data module

--- code/iginx-assoc-backend/src/main/java/com/xmu/iginx/assoc/modules/data/service/impl/DataImportServiceImpl.java
+++ code/iginx-assoc-backend/src/main/java/com/xmu/iginx/assoc/modules/data/service/impl/DataImportServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Locale;
 
 /**
